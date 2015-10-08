007e9a6c1f47f6ab8edbf25723f852266aba43f3
zhanglei <zhangleipro@163.com>
2015-10-08 16:10:44 +0800

    首次提交

---
+++ src/main/java/com/platform/mongo/s1/MongoDirver.java
@@ -0,0 +1,13 @@
+package com.platform.mongo.s1;
+
+public class MongoDirver {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
