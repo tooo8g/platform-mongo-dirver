10a2bbf14fef25b896192c041b8adcd049ca7545
zhanglei <zhangleipro@163.com>
2015-10-08 17:04:01 +0800

    常量类

---
+++ src/main/java/com/platform/mongo/util/Constant.java
@@ -0,0 +1,26 @@
+package com.platform.mongo.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Constant {
+
+	public static String uri;
+
+	static {
+		try {
+			Properties properties = new Properties();
+			InputStream inputstream = Thread.currentThread()
+					.getContextClassLoader()
+					.getResourceAsStream("mongo.properties");
+			properties.load(inputstream);
+			inputstream.close();
+
+			uri = properties.getProperty("uri");
+		} catch (IOException e) {
+
+		}
+	}
+
+}
