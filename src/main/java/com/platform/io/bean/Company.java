86e8e28144f07fbea1db992e5f331614d8c434bd
niyn <niyn>
2016-03-30 17:03:32 +0800

    权限分配

---
+++ src/main/java/com/platform/io/bean/Company.java
@@ -0,0 +1,56 @@
+package com.platform.io.bean;
+
+public class Company {
+	    
+	private String com_name;//公司名称
+	private String org_code;//组织机构代码
+	private String com_addr;//公司地址 
+	private String con_person;//联系人
+	private String con_way;//联系方式
+//	private String add_time;//注册时间
+	
+	
+//	public String getCom_filed() {
+//		return com_filed;
+//	}
+//	public void setCom_filed(String com_filed) {
+//		this.com_filed = com_filed;
+//	}
+//	public String getAdd_time() {
+//		return add_time;
+//	}
+//	public void setAdd_time(String add_time) {
+//		this.add_time = add_time;
+//	}
+	public String getCom_name() {
+		return com_name;
+	}
+	public void setCom_name(String com_name) {
+		this.com_name = com_name;
+	}
+	public String getOrg_code() {
+		return org_code;
+	}
+	public void setOrg_code(String org_code) {
+		this.org_code = org_code;
+	}
+	public String getCom_addr() {
+		return com_addr;
+	}
+	public void setCom_addr(String com_addr) {
+		this.com_addr = com_addr;
+	}
+	public String getCon_person() {
+		return con_person;
+	}
+	public void setCon_person(String con_person) {
+		this.con_person = con_person;
+	}
+	public String getCon_way() {
+		return con_way;
+	}
+	public void setCon_way(String con_way) {
+		this.con_way = con_way;
+	}
+	
+}
